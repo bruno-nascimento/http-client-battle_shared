af5c04945bf2917b3789de3d979cd04b5049f46b
Bruno Caldeira do Nascimento <bruno@localhost.localdomain>
2018-07-25 17:07:19 -0300

    init

---
+++ src/main/java/br/com/br/com/labbs/workout/httpclientbattle/shared/HttpClient.java
@@ -0,0 +1,23 @@
+package br.com.br.com.labbs.workout.httpclientbattle.shared;
+
+import io.opentracing.propagation.TextMap;
+
+public interface HttpClient {
+
+    int hit(int requestNumber);
+    String getClientName();
+    TextMap requestBuilderCarrier(final Object request); // {
+//        return new TextMap() {
+//            @Override
+//            public Iterator<Map.Entry<String, String>> iterator() {
+//                throw new UnsupportedOperationException("carrier is write-only");
+//            }
+//
+//            @Override
+//            public void put(String key, String value) {
+//                request.addHeader(key, value);
+//            }
+//        };
+//    }
+
+}
