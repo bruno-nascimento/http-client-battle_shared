c704f56dd4e121eb6acd1da1ec24c49e82ac7015
Bruno Caldeira do Nascimento <bruno@localhost.localdomain>
2018-07-26 11:12:06 -0300

    tracing

--- src/main/java/br/com/labbs/workout/httpclientbattle/shared/Tracing.java
+++ src/main/java/br/com/labbs/workout/httpclientbattle/shared/Tracing.java
@@ -6,21 +6,19 @@ import io.jaegertracing.internal.samplers.ConstSampler;
 
 public class Tracing {
 
+    private static final Configuration.SamplerConfiguration samplerConfig = Configuration.SamplerConfiguration.fromEnv()
+            .withType(ConstSampler.TYPE)
+            .withParam(1);
+    private static final Configuration.ReporterConfiguration reporterConfig = Configuration.ReporterConfiguration.fromEnv()
+            .withLogSpans(true);
+    private static final Configuration config = Configuration.fromEnv()
+            .withSampler(samplerConfig)
+            .withReporter(reporterConfig);
+
     private Tracing() {
     }
 
-    public static JaegerTracer init() {
-        Configuration.SamplerConfiguration samplerConfig = Configuration.SamplerConfiguration.fromEnv()
-                .withType(ConstSampler.TYPE)
-                .withParam(1);
-
-        Configuration.ReporterConfiguration reporterConfig = Configuration.ReporterConfiguration.fromEnv()
-                .withLogSpans(true);
-
-        Configuration config = Configuration.fromEnv()
-                .withSampler(samplerConfig)
-                .withReporter(reporterConfig);
-
+    public static JaegerTracer get() {
         return config.getTracer();
     }
 
